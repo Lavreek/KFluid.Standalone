5dc81e2560e66cf34eaf03d8f20142e872befc9f
lavreek <lavreeksolacki@yandex.ru>
2022-06-15 12:48:58 +0300

    preview ready

--- custom.js
+++ custom.js
@@ -1,12 +1,30 @@
+$('.card-question').on('click', function(event) {
+    if (event.currentTarget.className.includes("active"))
+        $(this).removeClass("active");
+    else
+        $(this).toggleClass('active');
+})
+
 $("#nav ul li a[href^='#']").on('click', function(e) {
     e.preventDefault();
 
     var hash = this.hash;
 
-    $('html, body').animate({
-        scrollTop: $(hash).offset().top
-    }, 300, function() {
+    $('html, body').animate(
+        {
+            scrollTop: $(hash).offset().top
+        }, 500, function()
+            {
+                window.location.hash = hash;
+            }
+    );
+});
 
-        window.location.hash = hash;
-    });
+window.addEventListener('scroll', function() {
+    let scrollToTop = document.querySelector('.bi-arrow-up-circle-fill');
+
+    if (pageYOffset > 600 && scrollToTop.style.opacity != .8)
+        scrollToTop.style.opacity = .8;
+    if (pageYOffset < 600 && scrollToTop.style.opacity != 0)
+        scrollToTop.style.opacity = 0;
 });
