ff0857e553b184616c3a71f633b9d3c884bd042f
lavreek <lavreeksolacki@yandex.ru>
2022-06-16 17:09:20 +0300

    upload and fix

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php 
-    if ($_SERVER['REQUEST_SCHEME'] == "http")
-        echo "<script>window.location.replace('https://".$_SERVER['SERVER_NAME']."/".$_SERVER['PATH_INFO']."');</script>";
+<?php
+    if ($_SERVER['REMOTE_ADDR'] != "127.0.0.1")
+        if ($_SERVER['REQUEST_SCHEME'] == "http")
+            echo "<script>window.location.replace('https://".$_SERVER['SERVER_NAME']."/".$_SERVER['PATH_INFO']."');</script>";
         
-
     include __DIR__."/page.html";
