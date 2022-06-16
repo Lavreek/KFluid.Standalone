4536fdb3158a1cfa7205a19b1aec15ef12395c6e
lavreek <lavreeksolacki@yandex.ru>
2022-06-16 16:18:40 +0300

    fix

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php 
+    if ($_SERVER['REQUEST_SCHEME'] == "http")
+        echo "<script>window.location.replace('https://".$_SERVER['SERVER_NAME']."/".$_SERVER['PATH_INFO']."');</script>";
+        
+
     include __DIR__."/page.html";
